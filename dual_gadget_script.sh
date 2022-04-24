a2718781d5b1a45d0090aa37cd696074a7ea1382
Carlos Armendariz <carlosarmendariz57@gmail.com>
2022-04-24 13:55:43 -0400

    changes to dual script for testing

--- dual_gadget_script.sh
+++ dual_gadget_script.sh
@@ -42,15 +42,15 @@ echo "05010906a101050719e029e715002501750195088102750895018101750195030508190129
 # MOUSE
 ###############################################################################
 # Add functions here
-MOUSE_FUNCTIONS_DIR=$GADGET_PATH/functions/hid.usb1
-mkdir $MOUSE_FUNCTIONS_DIR
+#MOUSE_FUNCTIONS_DIR=$GADGET_PATH/functions/hid.usb1
+#mkdir $MOUSE_FUNCTIONS_DIR
 
-echo 1 > $MOUSE_FUNCTIONS_DIR/protocol
-echo 3 > $MOUSE_FUNCTIONS_DIR/report_length
-echo 1 > $MOUSE_FUNCTIONS_DIR/subclass
+#echo 1 > $MOUSE_FUNCTIONS_DIR/protocol
+#echo 3 > $MOUSE_FUNCTIONS_DIR/report_length
+#echo 1 > $MOUSE_FUNCTIONS_DIR/subclass
 
 # Write report descriptor
-echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x03\\x15\\x00\\x25\\x01\\x95\\x03\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x05\\x81\\x03\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x02\\x81\\x06\\xc0\\xc0 > $MOUSE_FUNCTIONS_DIR/report_desc
+#echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x03\\x15\\x00\\x25\\x01\\x95\\x03\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x05\\x81\\x03\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x02\\x81\\x06\\xc0\\xc0 > $MOUSE_FUNCTIONS_DIR/report_desc
 
 ###############################################################################
 
@@ -64,7 +64,7 @@ echo 200 > $CONFIGS_DIR/MaxPower # 200 mA
 echo "Keyboard configuration" > $CONFIGS_DIR/strings/0x409/configuration
 
 # Link HID functions to configuration
-ln -s $MOUSE_FUNCTIONS_DIR $CONFIGS_DIR
+#ln -s $MOUSE_FUNCTIONS_DIR $CONFIGS_DIR
 ln -s $KEYBOARD_FUNCTIONS_DIR $CONFIGS_DIR
 
 ls /sys/class/udc > $GADGET_PATH/UDC
